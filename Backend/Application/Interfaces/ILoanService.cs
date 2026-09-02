8cf0fd5f007452d8597d364458b3550557b8610b
fernandovev <gualterbelmonte@gmail.com>
2026-09-01 22:51:53 -0400

    feat: agregar servicio para consulta de libros prestados

---
+++ Backend/Application/Interfaces/ILoanService.cs
@@ -0,0 +1,9 @@
+﻿using Backend.Domain.Models;
+
+namespace Backend.Application.Interfaces
+{
+    public interface ILoanService
+    {
+        Task<IEnumerable<LoanedBook>> GetLoanedBooksAsync();
+    }
+}
---
+++ Backend/Application/Services/LoanService.cs
@@ -0,0 +1,21 @@
+﻿using Backend.Application.Interfaces;
+using Backend.Domain.Interfaces;
+using Backend.Domain.Models;
+
+namespace Backend.Application.Services
+{
+    public class LoanService : ILoanService
+    {
+        private readonly ILoanRepository _loanRepository;
+
+        public LoanService(ILoanRepository loanRepository)
+        {
+            _loanRepository = loanRepository;
+        }
+
+        public async Task<IEnumerable<LoanedBook>> GetLoanedBooksAsync()
+        {
+            return await _loanRepository.GetLoanedBooksAsync();
+        }
+    }
+}
