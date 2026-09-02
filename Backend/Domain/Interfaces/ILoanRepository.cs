9d880f085c80044fe49207214b9a459956a4e719
fernandovev <gualterbelmonte@gmail.com>
2026-09-01 22:47:01 -0400

    feat: agregar interfaz de repositorio para libros prestados

---
+++ Backend/Domain/Interfaces/ILoanRepository.cs
@@ -0,0 +1,9 @@
+﻿using Backend.Domain.Models;
+
+namespace Backend.Domain.Interfaces
+{
+    public interface ILoanRepository
+    {
+        Task<IEnumerable<LoanedBook>> GetLoanedBooksAsync();
+    }
+}
