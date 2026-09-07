01080e594685d9bbfe6328467209c2ed9b2f2120
Wenba1 <balderramawendym@gmail.com>
2026-09-07 05:51:27 -0400

    feat>: aparece el boton de catalogo

--- Backend/Infraestructure/Persistence/LibraryDbContext.cs
+++ Backend/Infraestructure/Persistence/LibraryDbContext.cs
@@ -43,7 +43,7 @@ public partial class LibraryDbContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseMySql("server=localhost;database=librarydb;uid=root;pwd=yosoydios", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.46-mysql"));
+        => optionsBuilder.UseMySql("server=localhost;database=librarydb;uid=root;pwd=wendy10", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.46-mysql"));
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
